7f0d0d7ea404898fe5a36f1e772341111b0cfc63
soso5532@gmail.com <soso5532@gmail.com@da92dc24-d001-af0b-28ff-b56befdf819a>
2011-12-29 07:19:54 +0000

    플래그 추가할때 마지막 플래그는 파란색으로 표시 파란색 플래그 터치하면 지워지고 남은 플래그들 중 마지막 플래그를 파란색으로 변경 기존 vertices 라는 ArrayList<GeoPoint> 오브젝트 중심의 시스템을 flagIO 라는 RemovableItemizedOverlay 오브젝트로 다 바꿈

--- Virtual Land/src/org/iptime/chwang/virtual_land/RemovableItemizedOverlay.java
+++ Virtual Land/src/org/iptime/chwang/virtual_land/RemovableItemizedOverlay.java
@@ -5,16 +5,36 @@ import android.util.Log;
 
 public class RemovableItemizedOverlay extends MyIconItemizedOverlay {
 
+	Drawable drawable_blueflag;
+	
+	/*
 	public RemovableItemizedOverlay(Drawable defaultMarker){
 		super(boundCenterBottom(defaultMarker));
 		//super(defaultMarker);
+	}
+	*/
+	public RemovableItemizedOverlay(Drawable defaultMarker){
+		super(boundCenterBottom(defaultMarker));
+		drawable_blueflag=defaultMarker;
 	}
 	
 	@Override
 	protected boolean onTap(int index){
 		if(index==size()-1){
 			Log.i("Chwang","Here I will insert removing code");
+			Log.i("Chwang",Integer.toString(index));
 			
+			mOverlays.remove(index);
+			
+			Log.i("Chwang",Integer.toString(index));
+			
+			if(size()!=0) mOverlays.get(index-1).setMarker(drawable_blueflag);
+			
+			Log.i("Chwang",Integer.toString(index));
+			
+			//populate();
+			
+			Log.i("Chwang","After populate()");
 		}
 		
 		return true;
