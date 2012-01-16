b7ba8d9f7bddeee6b725e296bc1ddc5b1ddca3cf
soso5532@gmail.com <soso5532@gmail.com@da92dc24-d001-af0b-28ff-b56befdf819a>
2012-01-16 15:37:36 +0000

    FlagOverlay 이식 및 PolyLine을 FlagOverlay 안으로 넣음 CustomListAdapter 추가 (크기조절 미완성)

--- trunk/Virtual Land/src/org/iptime/chwang/virtual_land/MissionActivity.java
+++ trunk/Virtual Land/src/org/iptime/chwang/virtual_land/MissionActivity.java
@@ -1,22 +1,44 @@
 package org.iptime.chwang.virtual_land;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MissionActivity extends Activity {
 	
-	private String[] missions={"Taking Photos","Grapping flags", "String matching"};
-	private ListView list;
+	private ArrayList<Custom_List_Data> Array_Data;
+	private Custom_List_Data data;
+	private CustomListAdapter adapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mission);
 
-		list=(ListView)findViewById(R.id.mission_list);
-		list.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,missions));
+		Log.i("Chwang","Before Array_Data initialized");
+		
+		Array_Data=new ArrayList<Custom_List_Data>();
+		
+		Log.i("Chwang","After Array_Data initialized");
+		
+		data=new Custom_List_Data(R.drawable.icon,"첫번째 미션입니다","첫번째 미션 간략 설명");
+		Array_Data.add(data);
+		
+		data=new Custom_List_Data(R.drawable.check_redflag,"두번째 미션입니다","두번째 미션 간략 설명");
+		Array_Data.add(data);
+		
+		Log.i("Chwang","After add datas");
+		
+		ListView custom_list=(ListView)findViewById(R.id.Custom_List);
+		Log.i("Chwang","After custom_list set");
+		adapter=new CustomListAdapter(this,android.R.layout.simple_list_item_1,Array_Data);
+		Log.i("Chwang","After adapter initialized");
+		custom_list.setAdapter(adapter);
+		Log.i("Chwang","After setAdapter() called");
 		
 	}
 }
